149c01970c208300f480897cce5dad1640f52a11
lethanhkhoi2002 <86721132+lethanhkhoi2002@users.noreply.github.com>
2021-06-30 19:21:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'ê Thơ!',
     introDesc: `tao hỏi mày một chút 
-    nha
-    
-    `,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    nha`,
+    btnIntro: 'được thôi',
+    title: 'mày có muốn tao ngừng thích mày không',
+    desc: 'hay chọn đáp án mày muốn',
+    btnYes: 'không hãy cứ thích tao',
+    btnNo: 'có, mày yêu người khác đi :(',
+    question:'tại sao mày lại không muốn vậy <3',
+    btnReply: 'trả lời đi <3',
+    reply: 'vậy à nếu vậy tao sẽ tiếp tục thích mày',
+    mess: 'tao sẽ thích mày 🥰. tới khi mày có chồng thì thôi 😘😘',
+    messDesc: 'vậy nhá khi nào rảnh thì kéo rank LQ cho tao .',
+    btnAccept: 'nếu muốn nói gì thì nhắn tin cho tao <3',
+    messLink: 'https://www.facebook.com/mtplatao.sky.C9/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
